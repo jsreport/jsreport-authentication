4b4634285ae5388d6b34a0394128dccd667910aa
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-06-23 08:52:07 +0200

    make logout button working on subpath

--- studio/LogoutSettingsButton.js
+++ studio/LogoutSettingsButton.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Studio from 'jsreport-studio'
 
 export default class LogoutSettingsButton extends Component {
   render () {
     return <div>
       <div
         onClick={() => this.refs.logout.click()} style={{ cursor: 'pointer' }}>
-        <form method='POST' action='/logout'>
+        <form method='POST' action={Studio.resolveUrl('/logout')}>
           <input ref='logout' type='submit' id='logoutBtn' style={{ display: 'none' }} />
         </form>
         <i className='fa fa-power-off' /> Logout
